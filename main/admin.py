6814eb415d3e645b1f4aa124f22cab91340d2ca7
metoy <xpb.mrzt@gmail.com>
2023-03-09 18:06:02 +0600

    finished

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from main.models import Person, Post, Comment
+
 # Register your models here.
+
+
+admin.site.register(Person)
+admin.site.register(Comment)
+admin.site.register(Post)
--- main/models.py
+++ main/models.py
@@ -1,3 +1,42 @@
+import datetime
+
 from django.db import models
 
 # Create your models here.
+
+
+class Person(models.Model):
+    username = models.CharField(max_length=25)
+    name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField()
+    password = models.CharField(max_length=50)
+    age = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.username} | | {self.email} | | {self.age}'
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(Person, on_delete=models.CASCADE, related_name='post_author', null=False, blank=False)
+
+    def __str__(self):
+        return f'{self.title} | | {self.created_at}'
+
+
+class Comment(models.Model):
+    text = models.CharField(max_length=255)
+    author = models.ForeignKey(Person, on_delete=models.CASCADE, related_name='comment_author')
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='post_on')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.text} | | {self.author} | | {self.post}'
+
+
+class Home(models.Model):
+    text = models.TextField()
